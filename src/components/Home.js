3c4462cde1e3ee6eea9342bda60cfbc44472691b
Faizan Ahmad Khan <ahmadfaizan1604@gmail.com>
2021-05-16 18:49:58 +0530

    add-to-cart design done

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+function Home() {
+    return (
+        <div>
+            <div className="add-to-cart">
+                <img src="https://static.vecteezy.com/system/resources/thumbnails/000/496/007/small/Ecommerce_998.jpg" />
+            </div>
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-purple-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1617130317000" />
+                </div>
+                <div className="text-wrapper item">
+                    <span>
+                        I-Phone
+                    </span>
+                    <span>
+                        Price: $1000.00
+                    </span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button>Add To Cart</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home 
